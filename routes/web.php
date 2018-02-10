405e71be3fe785fa77cb4802a69e735283394506
vladimir.likhoded <vladimirlih@gmail.com>
2018-02-10 22:10:09 +0200

    create category.index page

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,8 @@
 */
 
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
-
     Route::get('/', 'DashboardController@dashboard')->name('admin.index');
-
+    Route::resource('/category', 'CategoryController', ['as' => 'admin']);
 });
 
 Route::get('/', function () {
